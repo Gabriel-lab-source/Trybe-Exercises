74a0016523abace5ab1fb70180972539e2f0f61a
Gabriel Amirati <gabriel_amirati@hotmail.com>
2023-05-21 12:24:22 -0300

    Exercicios finalizados

---
+++ Front-end/Seção 2 Introdução ao React/Dia 01 Hello world no React/exercicio-de-fixacao-2/src/About.js
@@ -0,0 +1,26 @@
+import { Component } from "react";
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Gabriel Amirati
+        </h1>
+        <p>
+          Prefiro programação orientada a objetos 
+        </p>
+        <h2>
+          Minhas habilidades
+        </h2>
+        <ul>
+          <li>Javascript</li>
+          <li>React</li>
+          <li>Testes</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default About;
---
+++ Front-end/Seção 2 Introdução ao React/Dia 01 Hello world no React/hello-world/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import HelloWorld from "./Hello-World";
+
+function App() {
+  return <HelloWorld />
+};
+
+export default App;
