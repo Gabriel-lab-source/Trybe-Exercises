ec9084b05c0cf7acb45c93844e711c72899e3460
Gabriel <gabriel_amirati@hotmail.com>
2023-09-28 13:16:40 -0300

    Store back-office completo

--- Front-end/Seção 2 Introdução ao React/Dia 07 Revisão Store Back Office/sd-035-exercise-store-back-office/src/App.tsx
+++ Front-end/Seção 2 Introdução ao React/Dia 07 Revisão Store Back Office/sd-035-exercise-store-back-office/src/App.tsx
@@ -1,10 +1,22 @@
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import ListProducts from './components/ListProducts';
 import RegisterProduct from './components/RegisterProduct';
 import './styles/App.css';
+import { ProductType, ProductWithId } from './types';
 
 function App() {
   const [switchComponent, setSwitchComponent] = useState(true);
+  const [products, setProducts] = useState<ProductWithId[]>([]);
+
+  const handleSubmit = (productInfo: ProductType) => {
+    const id = uuidv4();
+    setProducts([...products, { ...productInfo, id }]);
+  };
+
+  const handleDelete = (id: string | number) => {
+    setProducts(products.filter((product) => product.id !== id));
+  };
 
   return (
     <div className="app">
@@ -13,7 +25,11 @@ function App() {
         <button onClick={ () => setSwitchComponent(false) }>Ver produtos</button>
       </header>
       {
-        switchComponent ? <RegisterProduct /> : <ListProducts products={ [] } />
+        switchComponent ? <RegisterProduct
+          handleSubmit={
+          handleSubmit
+}
+        /> : <ListProducts products={ products } handleDelete={ handleDelete } />
       }
     </div>
   );
