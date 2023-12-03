76158968f7ce3d74d26c2a9b3ab5af57d28ad87c
Gabriel <gabriel_amirati@hotmail.com>
2023-12-03 16:08:11 -0300

    Exercício da sessão finalizado

---
+++ Front-end/Seção 6 Gerenciamento de estado com Redux/Dia 01 Introdução ao Redux - O estado global da aplicação/exercise-redux-intro/src/redux/actions/index.ts
@@ -0,0 +1,5 @@
+export const nextColor = () => ({ type: 'NEXT_COLOR' });
+
+export const previousColor = () => ({ type: 'PREVIOUS_COLOR' });
+
+export const randomColor = () => ({ type: 'RANDOM_COLOR' });
