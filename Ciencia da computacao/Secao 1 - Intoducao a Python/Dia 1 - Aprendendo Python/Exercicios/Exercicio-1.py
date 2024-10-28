bc3eaa13c99c31bf7a3ad44a781c21055cb78e0c
Gabriel Amirati <gabriel_amirati@hotmail.com>
2024-10-28 14:11:17 -0300

    Introdução a Python finalizada

---
+++ Ciencia da computacao/Secao 1 - Intoducao a Python/Dia 1 - Aprendendo Python/Exercicios/Exercicio-1.py
@@ -0,0 +1,5 @@
+def return_bigger_n(n1, n2):
+    return max(n1, n2)
+
+
+print(return_bigger_n(1, 4))
---
+++ Ciencia da computacao/Secao 1 - Intoducao a Python/Dia 1 - Aprendendo Python/Exercicios/Exercicio-3.py
@@ -0,0 +1,6 @@
+def square(n):
+    for row in range(n):
+        print(n * '*')
+
+
+print(square(4))
