79203faeefa573c0e09a4a3978d72ad4c1efc764
Gabriel Amirati <gabriel_amirati@hotmail.com>
2024-11-10 16:05:34 -0300

    Seção 2 finalizada

---
+++ Ciencia da computacao/Secao 2 - Algoritmos/Dia 1 - Complexidade de Algoritmos/Exemplos em aula/exemplo_complexidade.py
@@ -0,0 +1,24 @@
+def calculations(n):
+    number1 = 0
+    # Complexidade linear
+    for n1 in range(n):
+        number1 += n1
+
+    number2 = 0
+    # Complexidade quadrática
+    for n1 in range(n):
+        for n2 in range(n):
+            number2 += n1 + n2
+
+    number3 = 0
+    # Complexidade cúbica
+    for n1 in range(n):
+        for n2 in range(n):
+            for n3 in range(n):
+                number3 += n1 + n2 + n3
+
+    return number1, number2, number3
+
+
+n1, n2, n3 = calculations(100)
+print(f'{n1}, {n2}, {n3}')
